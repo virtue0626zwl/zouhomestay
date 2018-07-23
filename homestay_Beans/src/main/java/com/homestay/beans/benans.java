7f21f9218966282e1d845f885fc8d00a8c84180b
tom_glb <helloworld_glb@123.com>
2018-07-23 14:22:10 +0800

    test ZWL

--- homestay_Beans/src/main/java/com/homestay/beans/benans.java
+++ homestay_Beans/src/main/java/com/homestay/beans/benans.java
@@ -3,5 +3,9 @@ package com.homestay.beans;
 public class benans {
 	public void bar(){
 		System.out.println("hello");
+	
 	}
+	syso
+	
+
 }
--- homestay_Servlet/src/main/java/com/homestay/servlet/TestHello.java
+++ homestay_Servlet/src/main/java/com/homestay/servlet/TestHello.java
@@ -7,10 +7,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class TestHello {
 	@RequestMapping("/helloAction.action")
 	public void helloAction() throws CloneNotSupportedException{
-		System.out.println("你好啊,各位同学!哈哈哈你好啊啊啊");
+		System.out.println("浣犲ソ鍟�,鍚勪綅鍚屽!鍝堝搱鍝堜綘濂藉晩鍟婂晩");
 		System.out.println("woshi hhhhlidahu");
 		TestHello ss=new TestHello();
 		ss.clone();
 		  
+		System.out.println("名侦探柯南");
+		System.out.println("海贼王");
+		System.out.println("test");
+	
 	}
 }
